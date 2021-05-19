6bf56462d54c4a2adf6d504c0c444e79a77a7421
Chris Kujawa <ckujawa@chriskujawadev.com>
2021-05-19 14:00:05 -0400

    initial commit

---
+++ src/com/ckujawa/Hello.java
@@ -0,0 +1,17 @@
+package com.ckujawa;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println(helloJenkins());
+		System.out.println(helloJenkinsAgain());
+	}
+	
+	public static String helloJenkins() {
+		return "Hello Jenkins!";
+	}
+	
+	public static String helloJenkinsAgain() {
+		return "Hello Jenkins again!!!";
+	}
+
+}
